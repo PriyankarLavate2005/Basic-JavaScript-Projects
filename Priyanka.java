03cbef500dcf648b60a05fa68fad64741988b195
PriyankarLavate2005 <priyankalavate909@gmail.com>
2024-07-27 00:20:26 +0530

    Changes Saved Successfully

---
+++ Priyanka.java
@@ -0,0 +1,17 @@
+class Priyanka{
+    Priyanka(int a,int b){
+
+    System.out.println(a+b);
+    }
+    Priyanka(String x,String y){
+    System.out.println(x+y);
+    }
+
+    public static void main(String[] args){
+     Priyanka P =new Priyanka(23,56);
+     Priyanka s =new Priyanka("priyanka","Lavate");
+     
+   }
+
+
+   }
